a8ba091300dc964feb8b5e9111dfa50949d5466b
Pedro Torres <pedro.areal.torres@ing.com>
2024-01-23 11:06:47 +0100

    80. Remove Duplicates from Sorted Array II.ts

---
+++ 80. Remove Duplicates from Sorted Array II.ts
@@ -0,0 +1,10 @@
+function removeDuplicates(nums: number[]): number {
+  let j = 2;
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] !== nums[j - 2]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+}
