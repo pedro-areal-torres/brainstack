838b73db7f19067983a47c6da39bbcc8b8b3d862
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2024-10-07 22:34:39 +0200

    150. Duplicate Integer

---
+++ NeetCode/150. Duplicate Integer.ts
@@ -0,0 +1,16 @@
+class SolutionDI {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const seen: Set<number> = new Set();
+        for (const num of nums) {
+            if (seen.has(num)) {
+                return true; // Duplicate found, exit early
+            }
+            seen.add(num);
+        }
+        return false; // No duplicates found
+    }
+}
